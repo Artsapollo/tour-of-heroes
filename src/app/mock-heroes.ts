7734fd87bf861a10ac207dc77c968ac7de9b51b1
Artsapollo <lemart03@gmail.com>
2022-08-19 19:38:23 +0300

    Display a list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Strange'},
+  { id: 13, name: 'Spider-Man'},
+  { id: 14, name: 'Iron-Man'},
+  { id: 15, name: 'Captain America'},
+  { id: 16, name: 'Black Widow'},
+  { id: 17, name: 'Hulk'},
+  { id: 18, name: 'Hawkeye'},
+  { id: 19, name: 'Falcon'},
+  { id: 20, name: 'Winter soldier'}
+];
+
+
